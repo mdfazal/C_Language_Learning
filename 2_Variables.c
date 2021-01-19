20c355000624dfd65497954b00982c8b167d7e28
mdfazal <fazalmd964@gmail.com>
2021-01-19 17:36:42 +0530

    Learning Python

---
+++ 2_Variables.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+    char characterName [] = "John";
+    int characterAge = 35;
+    printf("There was once a man named %s\n", characterName);
+    printf("He was 70 years %d\n", characterAge);
+
+    characterAge = 30;
+    printf("He really like dthe name %s\n", characterName);
+    printf("But did not like being %d\n", characterAge);
+
+
+    return 0 ;
+}
