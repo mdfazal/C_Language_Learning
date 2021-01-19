fa828647778af87a454886c777b2a9f9605094e3
mdfazal <fazalmd964@gmail.com>
2021-01-19 17:38:21 +0530

    Learning Python

---
+++ 5_WorkingwithNumbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main ()
+{
+    printf("%f\n", 5.0 + 4.5);
+    printf("%f\n", 5 / 4.0);
+
+    int num = 6;
+    printf("%d\n", num);
+
+    printf("%f\n", pow(2, 3) );
+
+    printf("%f\n", ceil(36.356));
+    printf("%f", floor(36.356));
+
+    return 0;
+}
