602b312df199ca76794d9560c2e4ae7e90d639bc
mdfazal <fazalmd964@gmail.com>
2021-01-19 18:42:59 +0530

    Learning Python

--- 8_Getting_User_Input.c
+++ 8_Getting_User_Input.c
@@ -3,6 +3,16 @@
 
 int main()
 {
+    char name1[20];
+    printf("Enter your name: ");
+    fgets(name1, 20, stdin);
+    printf("Your name is %s\n", name1);
+    
+    char grade;
+    printf("Enter your grade: ");
+    scanf("%c", &grade);
+    printf("Your Grade is %c\n", grade);
+
     int age;
     printf("Entyer your age: ");
     scanf("%d", &age);
@@ -13,20 +23,10 @@ int main()
     scanf("%s", &name);
     printf("Your name is %s\n", name);
 
-    char name1[20];
-    printf("Enter your name: ");
-    fgets(name1, 20, stdin);
-    printf("Your name is %s\n", name1);
-
     double gpa;
     printf("Enter your Gpa: ");
     scanf("%lf", &gpa);
     printf("Your Gpa is  %f\n", gpa);
-
-    char grade;
-    printf("Enter your grade: ");
-    scanf("%c", &grade);
-    printf("Your Grade is %c", grade);
 
     return 0;
 }
