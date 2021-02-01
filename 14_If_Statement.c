96a9d164701fa374ed6b2aa336988ba875419f87
mdfazal <fazalmd964@gmail.com>
2021-02-01 19:41:03 +0530

    learn C

--- 14_If_Statement.c
+++ 14_If_Statement.c
@@ -11,8 +11,21 @@ int max(int num1, int num2){
     return result ;
 }
 
+int max1(int num3, int num4, int num5){
+    int result1;
+    if(num3 >= num4 && num3 >= num5){
+        result1 = num3;
+    } else if(num4 >= num3 && num4 >= num5){
+        result1 = num4;
+    } else {
+        result1 = num5;
+    }
+    return result1; 
+}
+
 int main()
 {
     printf("%d", max(4,10));
+    printf("%d", max1(10,2,3));
     return 0;
 }
