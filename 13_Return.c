d89c2908835c5e3d8438896fe8686ed180cc8101
mdfazal <fazalmd964@gmail.com>
2021-02-01 19:24:46 +0530

    learn C

--- 13_Return.c
+++ 13_Return.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+double cube(double num ){
+    double result = num*num*num;
+    return result;
+    printf("here");  //this never gets printed out becauzse after return the code breaks. 
+}
+
 int main()
 {
+    printf("Answer: %f", cube(3.0));
+    return 0;
+}
