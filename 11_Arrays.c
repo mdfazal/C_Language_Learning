500bde24224e8af1dc414d2b583b167a1897cc05
mdfazal <fazalmd964@gmail.com>
2021-02-01 17:56:38 +0530

    learn C

--- 11_Arrays.c
+++ 11_Arrays.c
@@ -6,5 +6,11 @@ int main()
     int luckyNumber[] = {4, 8, 15, 23, 42};
     luckyNumber[1] = 200;
     printf("%d", luckyNumber[1]);
+
+    int luck[10];
+    luck[1] = 8;
+    printf("%d", luck[1]);
+
+    char phrase[20];
     return 0;
 }
