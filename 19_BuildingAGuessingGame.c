4d883ea3b9deb883269fe2bb21e93a2ce0a530ea
mdfazal <fazalmd964@gmail.com>
2021-02-05 22:33:51 +0530

    learn C

--- 19_BuildingAGuessingGame.c
+++ 19_BuildingAGuessingGame.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main ()
+int main (){
+    int secretNumber = 5;
+    int guess;
+
+    while(guess != secretNumber){
+        printf("Enetr a number");
+        scanf("%d", &guess);
+
+    }
+
+    return 0;
+}
