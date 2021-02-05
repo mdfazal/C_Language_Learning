06eaecc916408ef5c95e32b622dd8c443f1aa745
mdfazal <fazalmd964@gmail.com>
2021-02-06 01:42:47 +0530

    learn C

--- 20_For_Loops.c
+++ 20_For_Loops.c
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 
 int main (){
+
+    int luckyNumbers[] = {4, 8, 15, 16, 23, 42};
     int i = 1;
-    for(i=1; i <=5; i++){
-        printf("%d\n", i);
+    for(i=0; i < 6; i++){
+        printf("%d\n", luckyNumbers[i]);
     }
     return 0;
 }
