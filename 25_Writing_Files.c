079283355020cb36435075168bed3f92153d3bcf
mdfazal <fazalmd964@gmail.com>
2021-02-06 23:21:33 +0530

    learn C

--- 25_Writing_Files.c
+++ 25_Writing_Files.c
@@ -2,4 +2,11 @@
 #include <stdlib.h>
 
 int main (){
+
+    FILE *fpointer = fopen("employees.txt", "w");
+    fprintf(fpointer, "Jim , Salesman \nPam, Receptionist \nOscar, Accounting");
+
+    fclose(fpointer);
+
+    return 0;
 }
